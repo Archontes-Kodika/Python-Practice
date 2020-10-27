4dc835ba84902eb2c1d16a3bc1b8d7bdad81e34f
Luis Vilches <luisvilhernandez@outlook.com>
2020-10-26 23:33:09 -0400

    Ejercicio-1 Twinkle done

---
+++ Exercise/Ejercicio-1/Twinkle.py
@@ -0,0 +1,4 @@
+x = ("Twinkle, twinkle, little star,\n")
+y = ("\tHow I wonder what you are! \n\t\tUp above the world so high,\n \t\tLike a diamond in the sky.\n")
+z = ("\tHow I wonder what you are")
+print(x + y + x + z)
